6bc3d0625a745be120b806f72474c0a61bdc3708
arijitdas122001 <aribwnlvpgbwn@gmail.com>
2024-02-15 11:33:12 +0530

    cors policy updated

--- client/src/Hooks/useFetch.js
+++ client/src/Hooks/useFetch.js
@@ -9,7 +9,7 @@ const useFetch = (url) => {
       const fetchData = async () => {
         setLoading(true);
         try {
-          const res = await axios.get(url,{withCredentials:true});
+          const res = await axios.get(url);
           setData(res.data);
         } catch (err) {
           setError(err);
